eb6c677c9f0543ab1f780aa5b62bbcbfd68616aa
Rahul Pandey <rahulrock.bgp@gmail.com>
2025-12-17 21:22:48 +0000

    Try fixing this error: `Runtime Error: Invariant failed: Could not find

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 import withPWA from '@ducanh2912/next-pwa';
 
@@ -37,6 +38,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  experimental: {
+    bundleClientPackages: ['recharts'],
   },
 };
 
