2f3407519853f264a5a93d7a3fb0a768df1006f7
Rahul Pandey <rahulrock.bgp@gmail.com>
2025-12-23 06:58:08 +0000

    Fix these errors
    Find in logs
    CtrlF
    Running build in Washington, D.C.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@
 import type {NextConfig} from 'next';
 import withPWA from '@ducanh2912/next-pwa';
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig: NextConfig = {
   serverActions: {
     bodySizeLimit: '8mb',
@@ -50,6 +43,11 @@ const nextConfig: NextConfig = {
   transpilePackages: ['recharts'],
 };
 
-export default pwaConfig(nextConfig);
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-    
+export default pwaConfig(nextConfig);
